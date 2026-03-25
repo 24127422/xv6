3a21f12e38f128cb64737b2b7661084f655e61dc
KhangUw <vominhkhang729@gmail.com>
2026-03-25 15:32:43 +0700

    fix xargs

--- user/xargs.c
+++ user/xargs.c
@@ -1,6 +1,6 @@
-#include "../kernel/types.h"
-#include "../kernel/param.h"
-#include "user.h"
+#include "kernel/types.h"
+#include "kernel/param.h"
+#include "user/user.h"
 
 int main(int argc, char *argv[]) {
     char *xargv[MAXARG];
@@ -8,13 +8,15 @@ int main(int argc, char *argv[]) {
     int start_idx = 1;
 
     if (argc < 2) {
-        printf("Usage: xargs command...\n");
+        fprintf(2, "Usage: xargs command...\n"); 
         exit(1);
     }
 
+    
     if (argc >= 3 && strcmp(argv[1], "-n") == 0) {
         start_idx = 3;
     }
+    
     
     int base_argc = 0;
     for (int i = start_idx; i < argc; i++) {
@@ -24,24 +26,28 @@ int main(int argc, char *argv[]) {
     int buf_idx = 0;
     char c;
     
+    
     while (read(0, &c, 1) == 1) {
         if (c == '\n') {
             buf[buf_idx] = '\0'; 
 
             
-            xargv[base_argc] = buf; 
-            xargv[base_argc + 1] = 0; 
-
             if (fork() == 0) {
-                exec(xargv[0], xargv);
-                printf("exec %s failed\n", xargv[0]);
+                
+                xargv[base_argc] = buf;
+                xargv[base_argc + 1] = 0; 
+                
+                exec(xargv[0], xargv); 
+                fprintf(2, "exec %s failed\n", xargv[0]);
                 exit(1);
             }
+            
             wait(0);
-
             buf_idx = 0; 
-        } else if (c != '\r') { 
-            buf[buf_idx++] = c;
+        } else {
+            if (buf_idx < sizeof(buf) - 1) {
+                buf[buf_idx++] = c;
+            }
         }
     }
     exit(0);
